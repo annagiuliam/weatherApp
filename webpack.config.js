a340f3bc16d5f7f5cb60db971034076362a3c833
annagiuliam <annagiulia.musso@gmail.com>
2021-08-18 16:22:47 +0200

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,14 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
 
 module.exports = {
-        mode: 'development',
-        entry: './src/index.js',
-        output: {
-                filename: 'main.js',
-                path: path.resolve(__dirname, 'dist'),
-        },
-        devtool: 'inline-source-map',
-        devServer: {
-                contentBase: './dist',
-        },
-        module: {
-                rules: [
-                        {
-                                test: /\.(s*)css$/,
-                                use: ExtractTextPlugin.extract({
-                                        fallback: 'style-loader',
-                                        use: ['css-loader', 'sass-loader'],
-                                }),
-                        },
-                        {
-                                test: /\.(png|jp(e*)g|svg)$/,
-                                use: [
-                                        {
-                                                loader: 'url-loader',
-                                                // options: {
-                                                //         limit: 8000, // Convert images < 8kb to base64 strings
-                                                //         name: 'images/[hash]-[name].[ext]',
-                                                // },
-                                        },
-                                ],
-                        },
-                ],
-        },
-        plugins: [new Dotenv()],
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
 };
