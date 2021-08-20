e731bec425b09e2b64fb3e2bf698e3cbc8842362
annagiuliam <annagiulia.musso@gmail.com>
2021-08-20 14:34:12 +0200

    added configs

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var path = require("path");
-var express = require("express");
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8080;
+const app = express();
 
-var app = express();
+app.use(express.static(__dirname + "/dist"));
 
-app.use(express.static(path.join(__dirname, "dist")));
-app.set("port", process.env.PORT || 8080);
-
-var server = app.listen(app.get("port"), function () {
-  console.log("listening on port ", server.address().port);
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
+
+app.listen(port);
+
+if (process.env.NODE_ENV == "development")
+  require("dotenv").config({ silent: true });
